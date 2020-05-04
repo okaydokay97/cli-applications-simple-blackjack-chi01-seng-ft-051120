9a064cbfbfcd4a2e353f33a76327d7d2ef90a2e8
Rubens Fernandes <rubens.fernandes97@gmail.com>
2020-05-04 16:14:07 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -19,8 +19,8 @@ def get_user_input
   gets.strip
 end
 
-def end_game
-  puts "Sorry, you hit"
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
