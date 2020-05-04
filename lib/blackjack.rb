fa33a4037c98294e439320c4b495703d0ef170a3
Rubens Fernandes <rubens.fernandes97@gmail.com>
2020-05-04 16:16:48 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -24,9 +24,8 @@ def end_game(card_total)
 end
 
 def initial_round
-  deal_card + deal_card
-  
-  binding.pry
+  card_total= deal_card + deal_card
+  display_card_total(card_total)
 end
 
 def hit?
